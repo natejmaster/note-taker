e36dcf70998bc6800623b16aee70a1d60a1551ee
natejmaster@gmail.com <natejmaster@gmail.com>
2023-08-27 10:32:25 -0500

    Added uuid helper function for ID generation, finished notes.js document

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,5 +1,5 @@
 const notes = require('express').Router();
-
+const uuid = require('../helpers/uuid');
 //GET Route for retrieving all notes
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
@@ -12,9 +12,17 @@ notes.post('/', (req, res) => {
     const { title, text } = req.body;
   
     if (req.body) {
-      const newTip = {
+      const newNote = {
         title,
         text,
-        tip_id: uuidv4(),
+        note_id: uuid(),
       };
   
+      readAndAppend(newNote, './db/db.json');
+      res.json(`Note added successfully`);
+    } else {
+      res.error('Error in adding note');
+    }
+  });
+  
+module.exports = notes;
