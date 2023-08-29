0f0129ed732e22f64e9aa02b041b14cc88bca1a9
natejmaster@gmail.com <natejmaster@gmail.com>
2023-08-29 18:41:41 -0500

    Fixed issue with rendering saved notes, added delete feature

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,6 +1,6 @@
 const notes = require('express').Router();
 const uuid = require('../helpers/uuid');
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
 //GET Route for retrieving all notes
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
@@ -16,7 +16,7 @@ notes.post('/', (req, res) => {
       const newNote = {
         title,
         text,
-        note_id: uuid(),
+        id: uuid(),
       };
   
       readAndAppend(newNote, './db/db.json');
@@ -25,5 +25,32 @@ notes.post('/', (req, res) => {
       res.error('Error in adding note');
     }
   });
+
+//DELETE Route for deleting a note:
+notes.delete('/:id', (req, res) => {
+    const noteIdToDelete = req.params.id;
   
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((notes) => {
+        // Find the index of the note to delete
+        const indexToDelete = notes.findIndex((note) => note.id === noteIdToDelete);
+  
+        if (indexToDelete !== -1) {
+          // Remove the note from the array
+          notes.splice(indexToDelete, 1);
+  
+          // Write the updated notes back to the file
+          writeToFile('./db/db.json', notes);
+  
+          res.json(`Note with ID ${noteIdToDelete} deleted successfully`);
+        } else {
+          res.status(404).json(`Note with ID ${noteIdToDelete} not found`);
+        }
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+
 module.exports = notes;
